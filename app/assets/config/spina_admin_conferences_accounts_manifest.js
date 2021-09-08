0f4efdd15e8cce45604f7046de8971eedb98ff31
Louis Van Steene <louis@lvs.me.uk>
2021-09-08 14:22:56 +0100

    Add javascript asset support and ES6 transpiler

--- app/assets/config/spina_admin_conferences_accounts_manifest.js
+++ app/assets/config/spina_admin_conferences_accounts_manifest.js
@@ -1,2 +1,3 @@
-//= link_directory ../stylesheets/spina/admin/conferences/accounts .css
+//= link spina/admin/conferences/accounts/application.js
+//= link spina/admin/conferences/accounts/application.css
 //= link spina_manifest.js
